810bc681884ff5b0248046a80c29dd6c04849944
samirdangal <samirdangal@gmail.com>
2020-07-06 16:05:26 +0545

    add flutter class (splash screen )

--- android_test/app/src/main/java/com/naxa/np/fluttermoduleandroidtest/MainActivity.java
+++ android_test/app/src/main/java/com/naxa/np/fluttermoduleandroidtest/MainActivity.java
@@ -2,13 +2,53 @@ package com.naxa.np.fluttermoduleandroidtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // This method is used so that your splash activity
+        // can cover the entire screen.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Defining the FlutterActivity to display
+        // the Flutter UI within this host app.
+        startActivity(
+                FlutterActivity
+                        .withNewEngine()
+                        .initialRoute("splashRoute")
+                        .build(this)
+        );
+
+
+        new Handler().postDelayed(
+                () -> {
+
+                    // This intent will be used to switch to the CounterActivity
+                    Intent i = new Intent(MainActivity.this,
+                            CounterActivity.class);
+
+                    // Invoke the CounterActivity
+                    startActivity(i);
+
+                    // The current activity will get finished
+                    finish();
+
+                }, SPLASH_SCREEN_TIME_OUT
+        );
+
+
     }
+
 }
